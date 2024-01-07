89e2c5cfa308ddc80942d8eb95564f7199260a43
Nyamadi Atsu <devbrainy98@gmail.com>
2024-01-07 12:26:33 +0100

    added auth error response status to auth middleware

--- backend/middleware/userAuth.js
+++ backend/middleware/userAuth.js
@@ -7,47 +7,48 @@ const authenticateUser = async (req, res, next) => {
     const authorization =
       req.headers?.authorization || req.headers?.Authorization;
     if (!authorization)
-      return res.status(403).json({ success: false, message: "Forbidden!" });
+      return res
+        .status(403)
+        .json({ success: false, authError: true, message: "Forbidden!" });
 
     const token = authorization.split(" ")[1];
 
     if (!token)
-      return res
-        .status(403)
-        .json({
-          success: false,
-          message: "No token found. Unauthorized access!",
-        });
+      return res.status(403).json({
+        success: false,
+        authError: true,
+        message: "No token found. Unauthorized access!",
+      });
 
     // verify token's validity
     const id = jwt.verify(token, process.env.JWT_SECRET_KEY);
     if (!id)
-      return res
-        .status(400)
-        .json({ success: false, message: "You are not authorized!" });
+      return res.status(400).json({
+        success: false,
+        authError: true,
+        message: "You are not authorized!",
+      });
 
-    const user = await User.findOne({ _id: id });
+    const user = await User.findOne({ _id: id }).lean().exec();
     if (!user)
-      res
-        .status(400)
-        .json({
-          success: false,
-          message: "User is either not authenticated or doesn't exist",
-        });
+      res.status(400).json({
+        success: false,
+        authError: true,
+        message: "User is either not authenticated or doesn't exist",
+      });
 
     //add user info except password to request headers
-    const { password, ...rest } = user._doc;
+    const { password, ...rest } = user;
     req.user = rest;
 
     //call the next function to execute the next middleware
     next();
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        success: false,
-        message: "Forbidden! Token expired or Not authorized!",
-      });
+    res.status(500).json({
+      success: false,
+      authError: true,
+      message: "Forbidden! Token expired or Not authorized!",
+    });
   }
 };
 
