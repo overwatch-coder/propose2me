1bee3460bae936d17f85d47c8435626796e772be
Nyamadi Atsu <devbrainy98@gmail.com>
2024-01-15 20:28:19 +0100

    added starting year in footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="py-3 mt-16 text-xs text-center text-white md:text-base bg-primary-main font-pacifico">
       <p className="flex items-center justify-center">
-        Copyright &copy; PTM {new Date().getFullYear()} All Rights Reserved.
+        Copyright &copy; PTM 2023 - {new Date().getFullYear()} All Rights Reserved.
       </p>
     </footer>
   );
